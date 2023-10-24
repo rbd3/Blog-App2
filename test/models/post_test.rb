85e25f3c5076f55945456db1cae0403590f8ba3e
rbd3 <rabedesanaandry01@gmail.com>
2023-10-24 12:01:21 +0300

    Add unit test for post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be valid with valid attributes' do
+    user = User.create(name: 'John')
+    post = Post.new(author: user, title: 'Hello', comments_counter: 0, likes_counter: 0)
+    assert post.valid?
+  end
+
+  test 'should not be valid without a title' do
+    user = User.create(name: 'Alice')
+    post = Post.new(author: user, comments_counter: 0, likes_counter: 0)
+    assert_not post.valid?
+  end
+
+  test 'title should not exceed 250 characters' do
+    user = User.create(name: 'Bob')
+    post = Post.new(author: user, title: 'A' * 251, comments_counter: 0, likes_counter: 0)
+    assert_not post.valid?
+  end
+
+  test 'comments_counter should be an integer greater than or equal to zero' do
+    user = User.create(name: 'Eve')
+    post = Post.new(author: user, title: 'Example', comments_counter: -1, likes_counter: 0)
+    assert_not post.valid?
+  end
+
+  test 'likes_counter should be an integer greater than or equal to zero' do
+    user = User.create(name: 'Frank')
+    post = Post.new(author: user, title: 'Test', comments_counter: 0, likes_counter: -1)
+    assert_not post.valid?
+  end
+
+  test 'increment_user_posts_counter should update user posts_counter' do
+    user = User.create(name: 'Jane', posts_counter: 0)
+    post = Post.create(author: user, title: 'Sample', comments_counter: 0, likes_counter: 0)
+    
+    user.reload # Reload the user to get the updated posts_counter value
+
+    assert_equal 1, user.posts_counter
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,18 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test 'should be valid with valid attributes' do
-    user = User.new(name: 'Mike Due', posts_counter: 0)
+  test 'recent_posts should return the 3 most recent posts' do
+    user = User.create(name: 'Hugo', posts_counter: 0)
+    post1 = user.posts.create(title: 'Post 1', created_at: 2.days.ago, comments_counter: 1, likes_counter: 1)
+    post2 = user.posts.create(title: 'Post 2', created_at: 1.day.ago, comments_counter: 1, likes_counter: 1)
+    post3 = user.posts.create(title: 'Post 3', created_at: Time.current, comments_counter: 1, likes_counter: 1)
+    post4 = user.posts.create(title: 'Post 4', created_at: 3.days.ago, comments_counter: 1, likes_counter: 1)
+
+    recent_posts = user.recent_posts
+
+    assert_equal [post3, post2, post1], recent_posts
+  end
+
+  test 'should be valid with a name and non-negative posts_counter' do
+    user = User.new(name: 'John Doe', posts_counter: 0)
     assert user.valid?
-  end
 
-  test 'should not be valid without a name' do
-    user = User.new(posts_counter: 0)
+    user.posts_counter = -1
     assert_not user.valid?
-  end
 
-  test 'should not be valid with a negative post counter' do
-    user = User.new(name: 'Kale', posts_counter: -1)
+    user.name = nil
     assert_not user.valid?
   end
 end

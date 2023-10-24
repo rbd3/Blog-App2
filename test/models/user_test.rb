fcb23db4cb9b5debdf647fb961e22cf92d1c734e
rbd3 <rabedesanaandry01@gmail.com>
2023-10-24 11:46:32 +0300

    Add unit test for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should be valid with valid attributes' do
+    user = User.new(name: 'Mike Due', posts_counter: 0)
+    assert user.valid?
+  end
+
+  test 'should not be valid without a name' do
+    user = User.new(posts_counter: 0)
+    assert_not user.valid?
+  end
+
+  test 'should not be valid with a negative post counter' do
+    user = User.new(name: 'Kale', posts_counter: -1)
+    assert_not user.valid?
+  end
 end

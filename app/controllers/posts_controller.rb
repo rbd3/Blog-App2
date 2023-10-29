d4d66a2f4d82139bae419b99d4f024c172af6c6b
rbd3 <rabedesanaandry01@gmail.com>
2023-10-29 17:05:23 +0300

    Add post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @comments = @post.comments
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end

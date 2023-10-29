17c28683e040f5d87c4bd08ed9b29a053942c547
rbd3 <rabedesanaandry01@gmail.com>
2023-10-29 17:20:57 +0300

    Fix linters error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,15 +8,13 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = User.find(params[:user_id])
     @comments = @post.comments
-
   end
 
   def new
     @post = Post.new
   end
 
-  def create
-  end
+  def create; end
 
   private
 

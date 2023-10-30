e98ed0f5637ab8e786852ed27a7e37bd00a01473
rbd3 <rabedesanaandry01@gmail.com>
2023-10-30 22:13:56 +0300

    Add rspec test for show page

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -19,4 +19,23 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('<h1>Posts index</h1>')
     end
   end
+
+  describe 'GET /posts/:id' do
+    let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+    let (:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
+    it 'returns http success' do
+      get user_post_path(user, post)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      get user_post_path(user, post)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct content on the user show page' do
+      get user_post_path(user, post)
+      expect(response.body).to include('<h1>Here is post list</h1>')
+    end
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -17,4 +17,23 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('<h1>Welcome</h1>')
     end
   end
+
+  describe 'GET /users/:id' do
+    let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+
+    it 'returns http success' do
+      get user_path(user)
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      get user_path(user)
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct content on the user show page' do
+      get user_path(user)
+      expect(response.body).to include('<h1>Here list of Users</h1>')
+    end
+  end
 end

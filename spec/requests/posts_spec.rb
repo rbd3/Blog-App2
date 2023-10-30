3d7fe6d43afcbb3612b1f302c55f78df5649f2f1
rbd3 <rabedesanaandry01@gmail.com>
2023-10-30 22:26:47 +0300

    Update test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /posts/index' do
+  describe 'GET /index' do
     user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
 
     it 'Return Http success' do
@@ -20,7 +20,7 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
-  describe 'GET /posts/show' do
+  describe 'GET /show' do
     let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
     let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
     it 'returns http success' do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /users/index' do
+  describe 'GET /index' do
     it 'returns http success' do
       get users_path
       expect(response).to have_http_status(200)
@@ -18,7 +18,7 @@ RSpec.describe 'Users', type: :request do
     end
   end
 
-  describe 'GET /users/show' do
+  describe 'GET /show' do
     let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
 
     it 'returns http success' do

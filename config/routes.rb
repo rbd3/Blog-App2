023d89473a37ac6e2b435beeaf80bf0b6f8759df
rbd3 <rabedesanaandry01@gmail.com>
2023-11-01 18:42:22 +0300

    Add form for new comment

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do 
     resources :posts, only: [:index, :show]
   end
+
+  resources :posts do
+    resources :comments, only: [:new, :create]
+    resource :likes, only: [:create]
+  end
 end

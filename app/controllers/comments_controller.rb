168680b99c80861e387083e8255ec20bf6f13669
rbd3 <rabedesanaandry01@gmail.com>
2023-11-01 22:11:55 +0300

    Fix new post error

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,23 @@
 class CommentsController < ApplicationController
   def new
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.new
   end
 
   def create
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user = current_user
-
+  
     if @comment.save
       redirect_to user_post_path(current_user, @post)
     else
+      flash[:alert] = 'Comment could not be saved'
+      puts @comment.errors.full_messages
       render :new
     end
+  end
+  
   end
 
   def comment_params

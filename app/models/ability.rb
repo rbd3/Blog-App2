337eb3bf1c35917eb1f046ffebe1eee0b46da1cc
noelfoka <nomgnenoel@gmail.com>
2023-11-09 15:39:41 +0100

    implement ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,14 @@
 class Ability
   include CanCan::Ability
 
+  def initialize(user)
+    user ||= User.new
+    can :read, User
+    can :read, Post
+    can :read, Comment
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+    can :manage, Post if user.is? :admin
+    can :manage, Comment if user.is?
+  end
 end
